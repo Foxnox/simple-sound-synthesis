10fe13c3d25796ad138cd1ec3c913d8fe66c7af9
Arnaud Delpeyroux <arnaud.delpeyroux@live.com>
2017-11-19 16:12:28 +0100

    correct all bugs, seems working (tested on firefox)

--- audio_libs/audio_synthesis.js
+++ audio_libs/audio_synthesis.js
@@ -10,7 +10,7 @@ var sinus_synthesis = function (args){
         return 0;
     }
     return osc_synthesis(args.a, args.f, args.t);
-}
+};
 
 var additive_synthesis = function (args){
     if (args.a == 0){
@@ -29,27 +29,10 @@ var am_synthesis = function (args){
 var fm_synthesis = function (args){
     if (args.a == 0){
         return 0;
-    }
+      ;    }
     return args.a * Math.sin((2 * Math.PI * args.fc * args.t) + osc_synthesis(args.I, args.fm, args.t));
-}
+};
 
 var calc_a = function (f, f0){
 	return ((2 * f0) - f)/f0;
-}
-
-var shepard_synthesis = function (args){
-	var ret = 0;
-    args2 = {}
-    
-	for (i = 1; i< 6; i++) {
-        args2.a1 = calc_a(args.f, 440);
-        args2.f1 = args.f * Math.pow(2, i) / 2;
-        args2.a2 =  1 - calc_a(args.f, 440);
-        args2.f2 = args.f / Math.pow(2, i);
-        args2.t = args.t
-		ret += additive_synthesis(args2);
-	}
-	ret /= 6;
-
-	return ret;
-}
+};
--- audio_libs/custom_audio_buffer.js
+++ audio_libs/custom_audio_buffer.js
@@ -1,107 +1,104 @@
 
 function CustomAudioBuffer (audioCtx) {
-    this.audioCtx = audioCtx;
-    
-    this.sampleRate = 44100;
-    this.length = Math.ceil(this.sampleRate);
-    
-    this.soundArray = new Float32Array(this.length);
+  this.audioCtx = audioCtx;
+  
+  this.sampleRate = 44100;
+  this.length = Math.ceil(this.sampleRate);
+  
+  this.soundArray = new Float32Array(this.length);
 
-    this.arrayBuffer = audioCtx.createBuffer(1, this.length, this.sampleRate);
-    this.arrayBuffer.copyToChannel(this.soundArray, 0, 0);
-    
-    this.audioBuffer = audioCtx.createBufferSource();
-    this.audioBuffer.loop = true;
-    this.audioBuffer.buffer = this.arrayBuffer;
-    
-    this.lastTimeUpdate = 0;
-    this.timeThreshold = 50;
-    
-    this.started = false;
+  this.arrayBuffer = audioCtx.createBuffer(1, this.length, this.sampleRate);
+  this.arrayBuffer.copyToChannel(this.soundArray, 0, 0);
+  
+  
+  this.lastTimeUpdate = 0;
+  this.timeThreshold = 50;
+  
+  this.started = false;
 
-    this.analyser = this.audioCtx.createAnalyser();
-    let visualizeContainer = document.getElementById("visualize");
-    this.visualizer = this.buildVisualizer(visualizeContainer.getBoundingClientRect().width, visualizeContainer.getBoundingClientRect().height, this.analyser);
-    
+  this.analyser = this.audioCtx.createAnalyser();
+  let visualizeContainer = document.getElementById("visualize");
+  this.visualizer = this.buildVisualizer(visualizeContainer.getBoundingClientRect().width, visualizeContainer.getBoundingClientRect().height, this.analyser);
+  
 
-    this.mode = "sin";
-    this.synthesisMethods = {
-        "sin" : sinus_synthesis,
-        "add" : additive_synthesis,
-        "am" : am_synthesis,
-        "fm" : fm_synthesis,
-        "shepard" : shepard_synthesis
-    }
+  this.mode = "sin";
+  this.synthesisMethods = {
+    "sin" : sinus_synthesis,
+    "add" : additive_synthesis,
+    "am" : am_synthesis,
+    "fm" : fm_synthesis
+  };
 };
 
 CustomAudioBuffer.prototype.updateSoundArray = function (args) {
-    var time = Date.now();
-    if (time - this.lastTimeUpdate >= this.timeThreshold){
-        for (index = 0; index < this.length; ++index){
-            args.t = index / this.sampleRate;
-            this.soundArray[index] = this.synthesisMethods[this.mode](args);
-        }
-        this.arrayBuffer.copyToChannel(this.soundArray, 0, 0);
-        this.lastTimeUpdate = Date.now();
+  var time = Date.now();
+  if (time - this.lastTimeUpdate >= this.timeThreshold){
+    console.log("ici");
+    for (let index = 0; index < this.length; ++index){
+      args.t = index / this.sampleRate;
+      this.soundArray[index] = this.synthesisMethods[this.mode](args);
     }
+    this.arrayBuffer.copyToChannel(this.soundArray, 0, 0);
+    this.lastTimeUpdate = Date.now();
+  }
 };
 
 CustomAudioBuffer.prototype.start = function() {
-    if (!this.started){
-        if (this.audioBuffer){
-            this.audioBuffer.connect(this.audioCtx.destination);
-            this.audioBuffer.connect(this.analyser);
-            this.audioBuffer.start();
-            this.visualizer.start();
-            this.started = true;
-        } else {
-            this.audioBuffer = this.audioCtx.createBufferSource();
-            this.audioBuffer.loop = true;
-            this.audioBuffer.buffer = this.arrayBuffer;
-            this.audioBuffer.connect(this.audioCtx.destination);
-            this.start();
-        }
-    } 
+  if (!this.started){
+    if (this.audioBuffer){
+      this.audioBuffer.connect(this.audioCtx.destination);
+      this.audioBuffer.connect(this.analyser);
+      this.audioBuffer.start();
+      this.visualizer.start();
+      this.started = true;
+    } else {
+      this.audioBuffer = this.audioCtx.createBufferSource();
+      this.audioBuffer.loop = true;
+      this.audioBuffer.buffer = this.arrayBuffer;
+      this.audioBuffer.connect(this.audioCtx.destination);
+      this.start();
+    }
+  } 
 };
 
 CustomAudioBuffer.prototype.stop = function () {
-    if (this.audioBuffer && this.started){
-        this.audioBuffer.stop();
-        this.visualizer.stop();
-        this.audioBuffer.disconnect(this.analyser);
-        this.analyser.disconnect(this.audioCtx.destination);
-        this.audioBuffer = null;
-        this.started = false;
-    }
+  if (this.audioBuffer && this.started){
+    this.audioBuffer.stop();
+    this.visualizer.stop();
+    this.audioBuffer.disconnect(this.analyser);
+    this.audioBuffer.disconnect(this.audioCtx.destination);
+    this.audioBuffer = null;
+    this.started = false;
+  }
 };
 
 CustomAudioBuffer.prototype.updateMode = function (mode){
-	this.mode = mode;
+  this.mode = mode;
 };
 
 CustomAudioBuffer.prototype.buildVisualizer = function(width, height, analyserNode) {
-    let self = this;
+  let self = this;
 
-    let visualizeZone = document.getElementById("visualisationTarget");
+  let visualizeZone = document.getElementById("visualisationTarget");
 
-    let canvasContext = visualizeZone.getContext("2d"); 
+  let canvasContext = visualizeZone.getContext("2d"); 
 
-    let canvasWidth = width;
-    visualizeZone.width = width;
+  let canvasWidth = width;
+  visualizeZone.width = width;
 
-    let canvasHeight = height;
-    visualizeZone.height = canvasHeight;
+  let canvasHeight = height;
+  visualizeZone.height = canvasHeight;
 
-    canvasContext.width = canvasWidth;
-    canvasContext.height = canvasHeight;
+  canvasContext.width = canvasWidth;
+  canvasContext.height = canvasHeight;
 
-    let canvasData = {
-        "canvas" : visualizeZone,
-        "width" : canvasWidth, 
-        "height" : canvasHeight 
-    };
+  let canvasData = {
+    "canvas" : visualizeZone,
+    "width" : canvasWidth, 
+    "height" : canvasHeight 
+  };
 
-    let visualizer = new SoundVisualizer(canvasData, analyserNode, 2048);
-    
-    return visualizer;
+  let visualizer = new SoundVisualizer(canvasData, analyserNode, 2048);
+  
+  return visualizer;
 };
